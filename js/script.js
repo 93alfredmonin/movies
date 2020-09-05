8a87eaa425add26c968c0a14124e631aea799fc4
Alfred <alfred.monin93@gmail.com>
2020-09-05 14:52:52 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+//alert('hello');
+
+//const result = confirm("Are you here");
+//console.log(result);
+
+//const ansver = prompt("Вам есть 18?","18");
+//console.log(ansver);
+
+const answers = [];
+
+//answers[0] = prompt('What is your name?','');
+//answers[1] = prompt('What is your sorname?','');
+//answers[2] = prompt('How old are you?','');
+
+console.log(typeof(answers));
